44477723dd73f6567b560dba08ff89e44448a4c4
Alex Dogoum <lexhoo@yahoo.fr>
2019-04-23 13:51:17 +0200

    hey

---
+++ mymovies/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FormEmployee from './FormEmployee';
+import './FormEmployee.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FormEmployee />
+      
+    </div>
+    
+  );
+}
+
+
+export default App;
